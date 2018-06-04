4d9601ab4a8c7e7ac61917350b99fdc8077d57f7
shabbir <shabbir.apsis@gmail.com>
2018-06-04 16:32:26 +0600

    sms into order

---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderDetail extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'order_details';
+
+    /**
+     * The database primary key value.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'orders_id',
+        'route_name',
+        'total_outlet',
+        'visited_outlet',
+        'order_details',
+        'created_by'
+    ];
+
+
+}
