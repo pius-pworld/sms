be93583ef05f62c36756a824b0ed54aec9eb5a6d
shabbir <shabbir.apsis@gmail.com>
2018-06-23 15:14:31 +0600

    sms outbox controller and model

---
+++ app/Models/SmsOutbox.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SmsOutbox extends Model
+{
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'sms_outboxes';
+
+    /**
+     * The database primary key value.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+    /**
+     * fillable
+     * @var array
+     */
+    protected $fillable=[
+        'sms_reciever_number',
+        'sms_content'
+    ];
+}
