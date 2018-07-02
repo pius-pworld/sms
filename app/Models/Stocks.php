ec03869512e915da3f3b0dc6f9391ce617296a85
shabbir <shabbir.apsis@gmail.com>
2018-07-02 18:27:31 +0600

    Stock

---
+++ app/Models/Stocks.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stocks extends Model
+{
+    //table name
+    protected  $tables="stocks";
+    //primary key
+    protected  $primaryKeys ="id";
+    //fillable
+    protected $fillable=[
+
+    ];
+}
