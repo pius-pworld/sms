aeb4c4739898bb5e0b7051b85f9d25b4e70f1880
shabbir <shabbir.apsis@gmail.com>
2018-07-05 17:00:40 +0600

    dropdown change conflict

--- app/Helper/DropdownHelper.php
+++ app/Helper/DropdownHelper.php
@@ -1,20 +1,4 @@
 <?php
-    function dropdownList($id)
-    {
-        $dd_info = DB::table('dropdown_list')->where('dd_id',$id)->first();
-        $data_info = DB::select($dd_info->query);
-        $res=[];
-        foreach ($data_info as $value){
-            $res[] = collect($value)->toArray();
-        }
-        $html = '<select class="form-control '.$dd_info->selector_class.'" name="'.$dd_info->field_name.'[]">';
-        $html .= '<option value="">Select One</option>';
-        foreach($res as $k=>$v)
-        {
-            $html .= '<option value="'.$v[$dd_info->option_id].'">'.$v[$dd_info->option_value].'</option>';
-        }
-        $html .= '</select>';
-        return $html;
 function dropdownList($id)
 {
     $dd_info = DB::table('dropdown_list')->where('dd_id',$id)->first();
@@ -23,7 +7,7 @@ function dropdownList($id)
     foreach ($data_info as $value){
         $res[] = collect($value)->toArray();
     }
-    $html = '<select class="form-control '.$dd_info->selector_class.'" name="'.$dd_info->field_name.'">';
+    $html = '<select class="form-control '.$dd_info->selector_class.'" name="'.$dd_info->field_name.'[]">';
     $html .= '<option value="">Select One</option>';
     foreach($res as $k=>$v)
     {
