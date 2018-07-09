3375179e9a81903294089b42640b644d5719e62b
Jakir Hosan <jakir.hosan@apsissolutions.com>
2018-07-09 14:55:09 +0600

    add condition for search element. add month picker

--- resources/views/grid/search_elements_all.blade.php
+++ resources/views/grid/search_elements_all.blade.php
@@ -1,37 +1,58 @@
 <div class="col-lg-12">
-    <div class="col-lg-4 form-group">
-        <label>Zone</label>
-        {!! dropdownList(1) !!}
-    </div>
-    <div class="col-lg-4 form-group">
-        <label>Region</label>
-        {!! dropdownList(2) !!}
-    </div>
-    <div class="col-lg-4 form-group">
-        <label>Teritory</label>
-        {!! dropdownList(3) !!}
-    </div>
-    <div class="col-lg-4 form-group">
-        <label>House</label>
-        {!! dropdownList(4) !!}
-    </div>
-    <div class="col-lg-4 form-group">
-        <label>ASO/SO</label>
-        {!! dropdownList(8) !!}
-    </div>
-    <div class="col-lg-4 form-group">
-        <label>Category</label>
-        {!! dropdownList(5) !!}
-    </div>
-    <div class="col-lg-4 form-group">
-        <label>Brand</label>
-        {!! dropdownList(6) !!}
-    </div>
-    <div class="col-lg-4 form-group">
-        <label>SKU</label>
-        {!! dropdownList(7) !!}
-    </div>
-
+    @if(isset($searchAreaOption['zone']) && $searchAreaOption['zone'] == 1)
+        <div class="col-lg-4 form-group">
+            <label>Zone</label>
+            {!! dropdownList(1) !!}
+        </div>
+    @endif
+    @if(isset($searchAreaOption['region']) && $searchAreaOption['region'] == 1)
+        <div class="col-lg-4 form-group">
+            <label>Region</label>
+            {!! dropdownList(2) !!}
+        </div>
+    @endif
+    @if(isset($searchAreaOption['territory']) && $searchAreaOption['territory'] == 1)
+        <div class="col-lg-4 form-group">
+            <label>Teritory</label>
+            {!! dropdownList(3) !!}
+        </div>
+    @endif
+    @if(isset($searchAreaOption['house']) && $searchAreaOption['house'] == 1)
+        <div class="col-lg-4 form-group">
+            <label>House</label>
+            {!! dropdownList(4) !!}
+        </div>
+    @endif
+    @if(isset($searchAreaOption['route']) && $searchAreaOption['route'] == 1)
+        <div class="col-lg-4 form-group">
+            <label>ASO/SO</label>
+            {!! dropdownList(8) !!}
+        </div>
+    @endif
+    @if(isset($searchAreaOption['category']) && $searchAreaOption['category'] == 1)
+        <div class="col-lg-4 form-group">
+            <label>Category</label>
+            {!! dropdownList(5) !!}
+        </div>
+    @endif
+    @if(isset($searchAreaOption['brand']) && $searchAreaOption['brand'] == 1)
+        <div class="col-lg-4 form-group">
+            <label>Brand</label>
+            {!! dropdownList(6) !!}
+        </div>
+    @endif
+    @if(isset($searchAreaOption['sku']) && $searchAreaOption['sku'] == 1)
+        <div class="col-lg-4 form-group">
+            <label>SKU</label>
+            {!! dropdownList(7) !!}
+        </div>
+    @endif
+    @if(isset($searchAreaOption['month']) && $searchAreaOption['month'] == 1)
+        <div class="col-lg-4 form-group">
+            <label>Month</label>
+            <input type="text" name="month" placeholder="Month" value="" class="form-control user-error monthpicker" aria-invalid="true">
+        </div>
+    @endif
     @if(isset($searchAreaOption['daterange']) && $searchAreaOption['daterange'] == 1)
         <div class="col-lg-4 form-group">
             <label>Date Range</label>
