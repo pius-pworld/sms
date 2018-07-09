fbfe773be5653edaf115735c34a793ebee80b7e6
shabbir <shabbir.apsis@gmail.com>
2018-07-09 11:43:38 +0600

    additional options common search panel

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -252,7 +252,7 @@ class ReportsController extends Controller
     public function houseStock(Request $request){
         $data['ajaxUrl'] = URL::to('house-stock-search');
         $data['searching_options'] = 'grid.search_elements_all';
-        $data['searchAreaOption'] = array('show'=>1,'daterange'=>1);
+        $data['searchAreaOption'] = array('show'=>1,'daterange'=>0);
         $memo = repoStructure();
         $data['memo_structure']= $memo;
         $data['level'] = 1;
@@ -297,7 +297,7 @@ class ReportsController extends Controller
     public function houseLifting(Request $request){
         $data['ajaxUrl'] = URL::to('house-lifting-search');
         $data['searching_options'] = 'grid.search_elements_all';
-        $data['searchAreaShow'] = 1;
+        $data['searchAreaOption'] = array('show'=>1,'daterange'=>0);
         $memo = repoStructure();
         $data['level'] = 2;
         $data['level_col_data'] =['Requested','Delivery'];
@@ -345,7 +345,7 @@ class ReportsController extends Controller
     public function routeWisePerformance(Request $request){
         $data['ajaxUrl'] = URL::to('db-wise-performance-search');
         $data['searching_options'] = 'grid.search_elements_all';
-        $data['searchAreaShow'] = 1;
+        $data['searchAreaOption'] = array('show'=>1,'daterange'=>0);
         $memo = repoStructure();
         $data['level'] = 3;
         $data['level_col_data'] =['Target','Sales','Ach%'];
