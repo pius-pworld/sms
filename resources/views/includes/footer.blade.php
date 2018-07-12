d0de1907e78c4010531ebee43aabf5163d41db62
Jakir Hosan <jakir.hosan@apsissolutions.com>
2018-07-12 13:41:02 +0600

    footer fixed

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,9 +1,13 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>Version</b> 1.0.1
-    </div>
-    <strong>Copyright &copy; 2018 <a target="_blank" href="http://www.apsissolutions.com">Apsis Solutions</a>.</strong> All rights
-    reserved.
+{{--<footer class="main-footer">--}}
+    {{--<div class="pull-right hidden-xs">--}}
+      {{--<b>Version</b> 1.0.1--}}
+    {{--</div>--}}
+    {{--<strong>Copyright &copy; 2018 <a target="_blank" href="http://www.apsissolutions.com">Apsis Solutions</a>.</strong> All rights--}}
+    {{--reserved.--}}
 
-</footer>
+{{--</footer>--}}
+
+
+
+
 
