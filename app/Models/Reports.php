e40d2bfde8d358ba9e6dc22ad66b94625c392e91
shabbir <shabbir.apsis@gmail.com>
2018-07-30 13:16:09 +0600

    house wise lifting error fixed

--- app/Models/Reports.php
+++ app/Models/Reports.php
@@ -65,36 +65,46 @@ class Reports extends Model
         $house_lifting_list=[];
         foreach ($ids as $house_key=>$house_value){
             $house = DistributionHouse::where('id',$house_value)->first()->toArray();
+            $selected_asm_rsm = User::where('territories_id',$house['territories_id'])->where('user_type','territory')->first();
             $sku_order_info=[];
             foreach ($selected_memo as $cat_key=>$cat_val){
                 $selected_skues=array_flatten($cat_val);
                 foreach ($selected_skues as $key => $value){
-                    $data = DB::table('order_details')
-                        ->select('brands.brand_name','skues.sku_name','order_details.short_name','order_details.quantity as oquantity','sale_details.quantity as salequantity')
-                        ->leftJoin('orders','orders.id','=','order_details.orders_id')
-                        ->leftJoin('sales',function($join){
-                            $join->on('sales.asm_rsm_id','=','orders.asm_rsm_id')
-                                ->on('sales.order_date','=','orders.order_date');
-                        })
-                        ->leftJoin('sale_details',function ($join){
-                            $join->on('sale_details.sales_id','=','sales.id')
-                                ->on('sale_details.short_name','=','order_details.short_name');
-                        })
-                        ->leftJoin('skues','skues.short_name','=','order_details.short_name')
-                        ->leftJoin('brands','brands.id','=','skues.brands_id')
-                        ->where('orders.order_type','Primary')
-                        ->where('orders.asm_rsm_id',$house['territories_id'])
-                        ->where('order_details.short_name',$value)
-                        ->orderBy('orders.id', 'DESC');
-                    if($data->exists()){
-                        $dt= $data->get()->toArray();
-                        $order_quantity = count_sku(array_column($dt,'oquantity'));
-                        $saler_quantity = count_sku(array_column($dt,'salequantity'));
-                        $sku_order_info[]=[
-                            $order_quantity,
-                            $saler_quantity
-                        ];
-                    }else{
+                    if(isset($selected_asm_rsm->id)){
+                        $data = DB::table('order_details')
+                            ->select('brands.brand_name','skues.sku_name','order_details.short_name','order_details.quantity as oquantity','sale_details.quantity as salequantity')
+                            ->leftJoin('orders','orders.id','=','order_details.orders_id')
+                            ->leftJoin('sales',function($join){
+                                $join->on('sales.asm_rsm_id','=','orders.asm_rsm_id')
+                                    ->on('sales.order_date','=','orders.order_date');
+                            })
+                            ->leftJoin('sale_details',function ($join){
+                                $join->on('sale_details.sales_id','=','sales.id')
+                                    ->on('sale_details.short_name','=','order_details.short_name');
+                            })
+                            ->leftJoin('skues','skues.short_name','=','order_details.short_name')
+                            ->leftJoin('brands','brands.id','=','skues.brands_id')
+                            ->where('orders.order_type','Primary')
+                            ->where('orders.asm_rsm_id',$selected_asm_rsm->id)
+                            ->where('orders.dbid',$house_value)
+                            ->where('order_details.short_name',$value)
+                            ->orderBy('orders.id', 'DESC');
+                        if($data->exists()){
+                            $dt= $data->get()->toArray();
+                            $order_quantity = count_sku(array_column($dt,'oquantity'));
+                            $saler_quantity = count_sku(array_column($dt,'salequantity'));
+                            $sku_order_info[]=[
+                                $order_quantity,
+                                $saler_quantity
+                            ];
+                        }
+                        else{
+                            $sku_order_info[]=[
+                                'N/R','N/P'
+                            ];
+                        }
+                    }
+                    else{
                         $sku_order_info[]=[
                             'N/R','N/P'
                         ];
